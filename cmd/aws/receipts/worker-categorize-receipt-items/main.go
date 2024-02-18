9c52c432905a52d0d332612b4373b979110780c2
Stefan Turanjanin <stefan.turanjanin@weareneopix.com>
2024-02-18 15:10:02 +0100

    Add categorize receipt items worker lambda

--- cmd/aws/receipts/worker-categorize-receipt-items/main.go
+++ cmd/aws/receipts/worker-categorize-receipt-items/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
+	"strings"
+
+	"github.com/sashabaranov/go-openai"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,39 +20,139 @@ import (
 	"github.com/steffanturanjanin/receipt-manager/internal/models"
 )
 
+const (
+	CATEGORIZE_RECEIPT_ITEMS_PROMPT_SERBIAN = `Dat je sledeći spisak kategorija: %s.
+Za svaki od sledećih artikala odrediti kojoj od navedenih kategorija pripada: %s.
+Svaki artikal pripada samo jednoj kategoriji. Rezultat vratiti u formatu JSON objekta, gde je ključ ime artikla,
+a vrednost ime kategorije kojoj artikal pripada. Zadržati imena artikala i kategorija u originalnom obliku.
+Ne dodavati nove kategorije i artikle.`
+)
+
+func GenerateCategorizationPrompt(categories []string, items []string) string {
+	categoriesParam := strings.Join(categories, ", ")
+	itemsParam := strings.Join(items, ", ")
+
+	return fmt.Sprintf(CATEGORIZE_RECEIPT_ITEMS_PROMPT_SERBIAN, categoriesParam, itemsParam)
+}
+
+var (
+	// Database instance
+	DB *gorm.DB
+
+	// OpenAI client
+	OpenAiClient *openai.Client
+)
+
 func init() {
 	// Initialize database
 	if err := db.InitializeDB(); err != nil {
 		os.Exit(1)
+	} else {
+		DB = db.Instance
 	}
+
+	// Initialize OpenAI Client
+	OpenAiClient = openai.NewClient(os.Getenv("OPEN_AI_API_KEY"))
 }
 
+// Process each individual message from the current SQS batch
 func processMessage(ctx context.Context, message events.SQSMessage) error {
-	receiptIdIntValue, err := strconv.Atoi(message.Body)
+	// Get Receipt ID from message body
+	receiptId, err := strconv.Atoi(message.Body)
 	if err != nil {
 		return err
 	}
 
-	receiptId := uint(receiptIdIntValue)
-
-	var receipt models.Receipt
-	if dbErr := db.Instance.Preload("ReceiptItems").First(&receipt, receiptId).Error; dbErr != nil {
+	// Retrieve Receipt Items related to Receipt ID
+	var dbReceiptItems []models.ReceiptItem
+	if dbErr := DB.Where("receipt_id = ?", receiptId).Find(&dbReceiptItems).Error; dbErr != nil {
 		return dbErr
 	}
 
-	//Categorize receipt items...
+	// Retrieve Categories
+	var dbCategories []models.Category
+	if dbErr := DB.Find(&dbCategories).Error; dbErr != nil {
+		return dbErr
+	}
 
-	log.Printf("Receipt: %v\n", receipt)
+	// Receipt receiptItems names
+	var receiptItems []string
+	for _, receiptItem := range dbReceiptItems {
+		receiptItem := receiptItem.Name
+		receiptItems = append(receiptItems, receiptItem)
+	}
 
-	//Update receipt items categories...
+	// Categories names
+	var categories []string
+	for _, dbCategory := range dbCategories {
+		category := dbCategory.Name
+		categories = append(categories, category)
+	}
+
+	// Send Completion request to OpenAI
+	response, err := OpenAiClient.CreateCompletion(ctx, openai.CompletionRequest{
+		Prompt:      GenerateCategorizationPrompt(categories, receiptItems),
+		Model:       openai.GPT3Dot5Turbo,
+		MaxTokens:   2000,
+		Temperature: 0.2,
+		Stop:        []string{"\n"},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// Get output
+	output := response.Choices[0].Text
+	// Normalize output
+	exp := regexp.MustCompile(`(\r\n?|\n){2,}`)
+	normalizedOutput := exp.ReplaceAllString(output, "$1")
+
+	var itemsCategoriesMap map[string]string
+	if err := json.Unmarshal([]byte(normalizedOutput), &itemsCategoriesMap); err != nil {
+		return err
+	}
+
+	// Update Receipt Items with Categories
+	for receiptItemName, categoryName := range itemsCategoriesMap {
+		var foundReceiptItem *models.ReceiptItem
+		var foundCategory *models.Category
+
+		// Find DB Receipt Item
+		for _, dbReceiptItem := range dbReceiptItems {
+			if dbReceiptItem.Name == receiptItemName {
+				foundReceiptItem = &dbReceiptItem
+				break
+			}
+		}
+
+		// Find DB Category
+		for _, dbCategory := range dbCategories {
+			if dbCategory.Name == categoryName {
+				foundCategory = &dbCategory
+				break
+			}
+		}
+
+		if foundReceiptItem != nil && foundCategory != nil {
+			foundReceiptItem.Category = foundCategory
+		}
+	}
+
+	// Bulk update ReceiptItems categories
+	if err := DB.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "id"}},
+		DoUpdates: clause.AssignmentColumns([]string{"category_id"}),
+	}).Create(&dbReceiptItems).Error; err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func handler(ctx context.Context, event events.SQSEvent) error {
 	for _, message := range event.Records {
-		err := processMessage(ctx, message)
-		if err != nil {
+		if err := processMessage(ctx, message); err != nil {
 			log.Printf("Error while trying to categorize receipt items: %+v\n", err)
 			continue
 		}
