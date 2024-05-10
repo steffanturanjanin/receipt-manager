8bebbaec4b7deb5fa188d57b9fb5bc5b8cd5b364
Stefan Turanjanin <stefan.turanjanin@weareneopix.com>
2024-05-10 19:03:43 +0200

    [nanoservices]: Update logout nanoservice

--- nanoservices/cmd/aws/auth/logout/main.go
+++ nanoservices/cmd/aws/auth/logout/main.go
@@ -3,25 +3,35 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/core"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 
-	"github.com/steffanturanjanin/receipt-manager/internal/auth"
 	"github.com/steffanturanjanin/receipt-manager/internal/controllers"
+	db "github.com/steffanturanjanin/receipt-manager/internal/database"
 	"github.com/steffanturanjanin/receipt-manager/internal/middlewares"
 )
 
 var (
-	authService   *auth.AuthService
-	gorillaLambda *gorillamux.GorillaMuxAdapter
+	// Database
+	DB *gorm.DB
+
+	// Router
+	GorillaLambda *gorillamux.GorillaMuxAdapter
 )
 
 func init() {
-	authService = auth.NewAuthService(nil)
+	// Initialize database
+	if err := db.InitializeDB(); err != nil {
+		os.Exit(1)
+	} else {
+		DB = db.Instance
+	}
 
 	// Build middleware chain
 	jsonMiddleware := middlewares.SetJsonMiddleware
@@ -31,22 +41,47 @@ func init() {
 
 	// Initialize router
 	router := mux.NewRouter()
-	router.HandleFunc("/auth/logout", handler)
-	gorillaLambda = gorillamux.New(router)
+	router.HandleFunc("/auth/logout", handler).Methods("POST")
+	GorillaLambda = gorillamux.New(router)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	authCookies := authService.Logout()
+	accessTokenCookie := http.Cookie{
+		Name:     "access_token",
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		Secure:   false,
+		HttpOnly: true,
+	}
 
-	http.SetCookie(w, (*http.Cookie)(&authCookies.AccessTokenCookie))
-	http.SetCookie(w, (*http.Cookie)(&authCookies.RefreshTokenCookie))
-	http.SetCookie(w, (*http.Cookie)(&authCookies.LoggedInCookie))
+	refreshTokenCookie := http.Cookie{
+		Name:     "refresh_token",
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		Secure:   false,
+		HttpOnly: true,
+	}
+
+	loggedInCookie := http.Cookie{
+		Name:     "logged_in",
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		Secure:   false,
+		HttpOnly: false,
+	}
+
+	http.SetCookie(w, (*http.Cookie)(&accessTokenCookie))
+	http.SetCookie(w, (*http.Cookie)(&refreshTokenCookie))
+	http.SetCookie(w, (*http.Cookie)(&loggedInCookie))
 
 	controllers.JsonResponse(w, nil, http.StatusOK)
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	r, err := gorillaLambda.ProxyWithContext(ctx, *core.NewSwitchableAPIGatewayRequestV1(&request))
+	r, err := GorillaLambda.ProxyWithContext(ctx, *core.NewSwitchableAPIGatewayRequestV1(&request))
 	return *r.Version1(), err
 }
 
